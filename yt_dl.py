0667469faa961971dc2c07eab01e902643deef26
var0s <63473442+var0s@users.noreply.github.com>
2022-05-17 19:03:41 -0300

    Add files via upload

---
+++ yt_dl.py
@@ -0,0 +1,1 @@
+from pytube import YouTubeURL = "URL"video = YouTube(URL)video_streams = video.streams.filter(file_extension='mp4').get_highest_resolution()name = video_streams.titlevideo_streams.download(filename = name + ".mp4",	output_path = "PATH")
